2de391b8989cf0fd9f2d261d767c2730ceebd3e0
wen_ndy <141882960+wendymorinigo777@users.noreply.github.com>
2023-11-23 21:11:52 -0300

    Create Usuarios.sql

---
+++ Usuarios.sql
@@ -0,0 +1,10 @@
+createTable.sql
+DROP TABLE IF EXISTS dbo.Cliente
+--Tabla Cliente
+CREATE TABLE dbo.Cliente (
+    id_Cliente INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    id_Persona INT FOREIGN KEY REFERENCES Persona(id_Persona) NOT NULL,
+    Fecha_Ingreso DATETIME NOT NULL DEFAULT(getdate()),
+	Calificacion VARCHAR(20) NOT NULL,
+	Estado VARCHAR(20) NULL
+);
